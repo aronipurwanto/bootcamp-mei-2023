baf97e645a194f504ba426e2118e09a19984b008
Roni Purwanto <ahmadroni@gmail.com>
2023-08-12 16:09:23 +0700

    feat: view list

--- spring-mvc/src/main/java/com/bootcamp/springmvc/controller/ProductController.java
+++ spring-mvc/src/main/java/com/bootcamp/springmvc/controller/ProductController.java
@@ -1,10 +1,13 @@
 package com.bootcamp.springmvc.controller;
 
+import com.bootcamp.springmvc.entity.ProductEntity;
 import com.bootcamp.springmvc.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/product")
@@ -18,6 +21,18 @@ public class ProductController {
     @GetMapping
     public ModelAndView index(){
         ModelAndView view = new ModelAndView("product/index");
+
+        String judul = "List Product";
+        view.addObject("dataJudul", judul);
+
+        // get data dari service, service => repo, repo => database
+        List<ProductEntity> dataProduct = service.getAll();
+        // kirim data ke view
+        view.addObject("listProduct", dataProduct);
+        // menghitung jumlah data
+        int jmlData = dataProduct.size();
+        // kirim data ke view
+        view.addObject("jmlData", jmlData);
         return view;
     }
 }
