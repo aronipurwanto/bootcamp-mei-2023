62eabac70c7f05ed01d939952a83c453de023099
Roni Purwanto <ahmadroni@gmail.com>
2023-09-04 19:48:29 +0700

    feat: fix password encoder

--- security-demo/src/main/java/com/bootcamp/securitydemo/dao/UserDao.java
+++ security-demo/src/main/java/com/bootcamp/securitydemo/dao/UserDao.java
@@ -1,9 +1,11 @@
 package com.bootcamp.securitydemo.dao;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Repository;
 
 import java.util.Arrays;
@@ -11,18 +13,23 @@ import java.util.Collections;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class UserDao {
-    private final static List<UserDetails> APPLICATION_USERS = Arrays.asList(
-            new User("admin@gmail.com","password", Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN"))),
-            new User("user@gmail.com","password", Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")))
-    );
+    private final PasswordEncoder passwordEncoder;
+
+    public List<UserDetails> getUserDetails(){
+        return Arrays.asList(
+                new User("admin@gmail.com",passwordEncoder.encode("password"), Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN"))),
+                new User("user@gmail.com",passwordEncoder.encode("password"), Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")))
+        );
+    }
 
     public UserDetails findUserByEmail(String email){
-        UserDetails user = APPLICATION_USERS.stream()
+        List<UserDetails> users = this.getUserDetails();
+        
+        return users.stream()
                 .filter(u -> u.getUsername().equals(email))
                 .findFirst()
                 .orElseThrow(() ->new UsernameNotFoundException("User does not exist..!"));
-
-        return user;
     }
 }
